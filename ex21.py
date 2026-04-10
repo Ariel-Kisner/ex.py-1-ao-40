308898814fa32ccc691f271c39ce1a3229b4623e
Leticia Kisner <leticia.candido.mendes@escola.pr.gov.br>
2026-04-10 09:40:50 -0300

    Create ex21.py

---
+++ ex21.py
@@ -0,0 +1,7 @@
+nota = float(input("Nota: "))
+frequencia = int(inpt("Frequêcia (%): "))
+
+if nota >= 7 or frequencia >= 90:
+  print("Situação especial de aprovação")
+else:
+  print("Aluno em avalição")
