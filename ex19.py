99f8ffacdfbb360588273d61d4553b8a0aef4f88
Leticia Kisner <leticia.candido.mendes@escola.pr.gov.br>
2026-04-10 09:32:56 -0300

    Create ex19.py

---
+++ ex19.py
@@ -0,0 +1,8 @@
+idade = int(input("idade: "))
+gestante = input("É gestante (sim ou não): ")
+
+if idade >=60 or geestante == "sim":
+  print("Senha preferencial")
+
+else:
+  print("Atemdimento normal")
